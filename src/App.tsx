15eb1c0a8e3a4d4dc309f2ff3e0bfcce91e90d7f
AmilMasic <amil.masic@gmail.com>
2025-03-25 16:39:23 -0400

    move getRandomMove outside App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import "./App.css";
 
 type Move = "rock" | "paper" | "scissors";
 const MOVES: readonly Move[] = ["rock", "paper", "scissors"];
+const getRandomMove = () => {
+  const randomIndex = Math.floor(Math.random() * MOVES.length);
+  return MOVES[randomIndex];
+};
 function App() {
   const [computerMove, setComputerMove] = useState<Move>("rock");
-  const getRandomMove = () => {
-    const randomIndex = Math.floor(Math.random() * MOVES.length);
-    return MOVES[randomIndex];
-  };
 
   useEffect(() => {
     setComputerMove(getRandomMove());
