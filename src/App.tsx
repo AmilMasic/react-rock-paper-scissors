5ee3e91effa61bd621916c5f0d010ab2572487be
AmilMasic <amil.masic@gmail.com>
2025-03-26 12:04:05 -0400

    add winning logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 
 const MOVES = ["rock", "paper", "scissors"] as const;
 type Move = (typeof MOVES)[number];
+
+const GAMERESULT = ["tie", "player", "computer"] as const;
+type GameResult = (typeof GAMERESULT)[number];
 const getRandomMove = () => {
 	const randomIndex = Math.floor(Math.random() * MOVES.length);
 	return MOVES[randomIndex];
@@ -11,15 +14,44 @@ const getRandomMove = () => {
 function App() {
 	const [computerMove, setComputerMove] = useState<Move>(getRandomMove());
 	const [playerMove, setPlayerMove] = useState<Move | null>(null);
+	const [gameResult, setGameResult] = useState<GameResult | null>(null);
+	const [winningMessage, setWinningMessage] = useState("");
 
 	const resetGame = () => {
 		setComputerMove(getRandomMove());
 		setPlayerMove(null);
 	};
 
+	const checkWinner = (playerMove: Move, computerMove: Move) => {
+		if (playerMove === computerMove) {
+			setGameResult("tie");
+		} else if (
+			(playerMove === "rock" && computerMove === "scissors") ||
+			(playerMove === "paper" && computerMove === "rock") ||
+			(playerMove === "scissors" && computerMove === "paper")
+		) {
+			setGameResult("player");
+		} else {
+			setGameResult("computer");
+		}
+	};
+
+	useEffect(() => {
+		if (playerMove !== null) {
+			checkWinner(playerMove, computerMove);
+			if (gameResult === "player") {
+				setWinningMessage("You chose... Well!");
+			} else if (gameResult === "computer") {
+				setWinningMessage("You chose... Poorly!!");
+			} else {
+				setWinningMessage("It's a tie! ");
+			}
+		}
+	}, [gameResult, playerMove, computerMove]);
+
 	return (
 		<div className="bg-gray-200 md:w-1/2 w-full mx-auto h-screen flex flex-col align-items-center *:mx-auto ">
-			<div className=" py-5 font-bold text-2xl  ">ROCK PAPER SCISSOR</div>
+			<div className=" py-5 font-bold text-2xl">ROCK PAPER SCISSOR</div>
 			<p className="py-5 font-semibold">
 				Hello Player, Choose your move. Choose wisely!
 			</p>
@@ -55,6 +87,15 @@ function App() {
 							<span className="font-bold ml-2">{computerMove}</span>
 						</div>
 					</div>
+					<h3
+						className={`font-bold text-2xl mb-5  ${
+							gameResult === "player" ? "text-green-500" : "text-red-500"
+						} 
+               ${gameResult === "tie" && "text-gray-500"}
+            `}
+					>
+						{winningMessage}
+					</h3>
 					<button
 						className="bg-gray-300 py-2 px-4 rounded-md min-w-24 hover:bg-slate-400"
 						onClick={resetGame}
